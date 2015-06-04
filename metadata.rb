0de8e597eaf10b1f211964e79526423a3254b78b
Konstantin Lysenko <konstantin@binary.com>
2015-06-04 17:34:30 +0800

    Add: cookbook skeleton

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'logentries_rsyslog_ng'
+maintainer       'Kostiantyn Lysenko'
+maintainer_email 'gshaud@gmail.com'
+license          'apache2'
+description      'Installs/Configures logentries_rsyslog_ng'
+long_description 'Installs/Configures logentries_rsyslog_ng'
+version          '0.0.1'
+
